641a69ae5140e3bc6771b99265cfb16c277d240b
xieyipeng <3239202719@qq.com>
2018-10-16 21:20:32 +0800

    10.16

---
+++ jar/src/SocketTest.java
@@ -0,0 +1,18 @@
+
+/**
+ * Created with IntelliJ IDEA.
+ * TODO:
+ * User: xieyipeng
+ * Date: 2018-08-20
+ * Time: 10:58
+ */
+public class SocketTest {
+    public static void main(String[] args) {
+//        Connection connection=myConnection.connect("xieyipeng","123456","design");
+//        if (connection!=null){
+//            System.out.println("123");
+//        }else {
+//            System.out.println("234");
+//        }
+    }
+}
